7281e2340695610d9281df670c78a10bb66cb69a
Jason Goemaat <jason.goemaat@gmail.com>
2022-05-14 13:47:52 -0500

    Trying out simple CounterComponent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <CounterComponent/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CounterComponent from './components/CounterComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CounterComponent
   }
 }
 </script>
---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="centered-list">
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="counter++">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloComponent",
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    border: 20px transparent;
+    border-radius: 15px;
+    background-color: bisque;
+    padding: 0.5em;
+    width: 320px;
+  }
+  button {
+    font-weight: bold;
+    font-size: 1.4em;
+    border-radius: 10px;
+    background-color: gold;
+    padding: 0.5em 1.5em;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: black;
+    color: gold;
+  }
+
+  div.centered-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  div.centered-list > * {
+    margin: 0.25em;
+  }
+</style>
