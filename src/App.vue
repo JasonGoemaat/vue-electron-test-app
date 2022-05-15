34a79123a9ca7a819ef3a010bcb78b18ec423526
Jason Goemaat <jason.goemaat@gmail.com>
2022-05-15 13:03:35 -0500

    All cleaned up, showing only openFile and handleOpenFile

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,19 @@
 <template>
-  <CounterComponent/>
   <ViewFile/>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import CounterComponent from './components/CounterComponent.vue'
 import ViewFile from './components/ViewFile'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    CounterComponent,
     ViewFile
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 button {
   border-width: 0.1em;
   border-radius: 0.5em;
